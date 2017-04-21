a2b8bed8213c503f6bdb471821070ce00e16d55c
Nicole Sanders <nsanders9022@gmail.com>
2017-04-21 09:11:55 -0700

    Add link from home index to product index page which displays a list of products

---
+++ src/GummiBear/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GummiBear.Models;
+
+
+namespace GummiBear.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private GummiBearDbContext db = new GummiBearDbContext();
+        public IActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+    }
+}
