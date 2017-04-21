1ca7b6784ef0e99645ebb570bba1ab4c17a4ce4a
Nicole Sanders <nsanders9022@gmail.com>
2017-04-21 13:39:01 -0700

    add styling to links and content divs on the home index page

--- src/GummiBear/Controllers/HomeController.cs
+++ src/GummiBear/Controllers/HomeController.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using GummiBear.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 
 namespace GummiBear.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /<controller>/
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
         {
             return View();
         }
